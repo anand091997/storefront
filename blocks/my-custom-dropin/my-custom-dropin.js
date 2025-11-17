4fde5cb011099f19f3b2b85694f08dc0258cd7eb
Anand Parmar <anand.parmar@brainvire.com@BVIAHMLAP-659.brainvire.com>
2025-11-17 17:32:44 +0530

    tab changes

--- blocks/my-custom-dropin/my-custom-dropin.js
+++ blocks/my-custom-dropin/my-custom-dropin.js
@@ -2,8 +2,6 @@ import { render as myRenderer } from '../../scripts/__dropins__/cus-dropin/rende
 import { LoginContainer } from '../../scripts/__dropins__/cus-dropin/containers/LoginContainer.js';
 
 export default async function decorate(block) {
-  console.log('Rendering custom drop-in...');
-  block.innerHTML = '';
+  block.innerHTML = ''; 
   await myRenderer.render(LoginContainer, {})(block);
-  console.log('Drop-in rendered ✅');
 }
